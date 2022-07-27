31c4d4fb55e5140f0b80b83aed6bed19086d21fb
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-07-27 08:27:11 -0700

    added reload function

--- main.js
+++ main.js
@@ -19,8 +19,50 @@ inputs.forEach((item) => {
 
 const submit = (e) => {
   e.preventDefault();
-  console.log("submit");
+  if (password.value !== confirmPassword.value) {
+    password.classList.add("error");
+    confirmPassword.classList.add("error");
+    errorMessage.textContent = "Passwords don't match";
+    return;
+  }
 };
+
+if (
+  !phone.value.match(
+    /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
+  )
+) {
+  phone.classList.add("error");
+  errorMessage.textContent =
+    "Phone number needs to be exactly 10 numbers in length.";
+  return;
+}
+if (password.value.length < 8) {
+  password.classList.add("error");
+  errorMessage.textContent = "Password needs to be at least 8 characters long.";
+  return;
+}
+if (!password.value.match(/[a-z]/)) {
+  password.classList.add("error");
+  errorMessage.textContent =
+    "Password needs to have at least 1 lower case letter.";
+  return;
+}
+if (!password.value.match(/[A-Z]/)) {
+  password.classList.add("error");
+  errorMessage.textContent =
+    "Password needs to have at least 1 upper case letter.";
+  return;
+}
+if (!password.value.match(/\d+/g)) {
+  password.classList.add("error");
+  errorMessage.textContent = "Password needs to have at least 1 number.";
+  return;
+}
+errorMessage.textContent = "Form added Successfully.";
+setTimeout(() => {
+  window.location.reload();
+}, 10000);
 
 const form = document.querySelector("form");
 form.addEventListener("submit", submit);
